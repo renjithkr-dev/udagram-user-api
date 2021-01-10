4d3edb134e452debfee8b126d1e2ce1c69e2808c
Renjith <renjithtech6@gmail.com>
2021-01-10 07:39:40 -0500

    changed CORS to accept all

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
+    origin: "*",
   }));
 
   app.use('/api/v0/', IndexRouter);
